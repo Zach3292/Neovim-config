bb51018df7ca2fe07e7b8be079c07aa5de4a0230
Zach3292 <Zach3292@hotmail.com>
2024-10-13 10:38:40 -0400

    backup of config

---
+++ lua/chadrc.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.ui = {
+  theme = "palenight",
+}
+M.plugins = "plugins"
+
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+    }
+  }
+}
+
+
+return M
